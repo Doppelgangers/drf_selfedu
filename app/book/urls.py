9ca4ea844ec8b1e6545f948c0f4632c9d7a30d52
Doppelgangers <6666genocide666@mail.ru>
2023-11-14 21:56:25 +0300

    hight level serializatior

--- app/book/urls.py
+++ app/book/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from book.views import BookAPIView
+from book.views import BookAPIList, BookAPIUpdate, BookDetailAPI
 
 urlpatterns = [
-    path('api/v1/bock/list', BookAPIView.as_view())
+    path('api/v1/bock', BookAPIList.as_view()),
+    path('api/v1/bock/<int:pk>', BookAPIUpdate.as_view()),
+    path('api/v1/bock/detail/<int:pk>', BookDetailAPI.as_view()),
+
 ]
