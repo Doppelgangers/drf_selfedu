dc87d59f6dd546f35ff9a3219ad6dec67ccee05b
Doppelgangers <6666genocide666@mail.ru>
2023-11-14 19:30:27 +0300

    low level serialization

--- app/book/serializers.py
+++ app/book/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
+
+
+from rest_framework.renderers import JSONRenderer
+import rest_framework
+
 from .models import Book
 
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ("title", "category_id")
+class BookSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=256)
+    description = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_active = serializers.BooleanField(default=True)
+    category_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_active = validated_data.get("is_active", instance.is_active)
+        instance.category_id = validated_data.get("category_id", instance.category_id)
+        instance.save()
+        return instance
--- app/book/views.py
+++ app/book/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from rest_framework import generics
-
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookAPIView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookAPIView(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+class BookAPIView(APIView):
+
+    def get(self, request):
+        lst = Book.objects.all()
+        return Response({"books": BookSerializer(lst, many=True).data})
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        new_post = Book.objects.create(
+            title=request.data["title"],
+            description=request.data["description"],
+            category_id=request.data["category_id"]
+        )
+        return Response({"created": BookSerializer(new_post).data})
+
